d8cd5cbc1f076e3be923d78e2c1e98b525534b8e
Vipin Kumar <vipin.kumar@softobiz.com>
2021-02-04 12:54:36 +0530

    added modal

--- apps/mf-project-app/src/app/components/project-info/project-info.tsx
+++ apps/mf-project-app/src/app/components/project-info/project-info.tsx
@@ -7,13 +7,30 @@ import AddProject from "../add-project/add-project";
 import Project from "../project/project";
 import { ITodo } from "../../interfaces/project"; 
 import { Button, Header, Segment, Icon } from 'semantic-ui-react';
+import Modal from 'react-modal';
+
+
 
 /* eslint-disable-next-line */
 export interface ProjectInfoProps {}
 
 export function ProjectInfo(props: ProjectInfoProps) {
+  const customStyles = {
+    content : {
+      //top                   : '50%',
+      left                  : '50%',
+      right                 : 0,
+      bottom                : 'auto',
+    //  marginRight           : '-50%',
+      //transform             : 'translate(-50%, -50%)'
+    }
+  };
+  
+  
   const { loading, error, data } = useTodoQuery(GET_TODOS);
   const [openForm, setopenForm] = React.useState(false);
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+
 
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>Something went wrong!</h1>;
@@ -22,12 +39,43 @@ export function ProjectInfo(props: ProjectInfoProps) {
     console.log('add=project')
     setopenForm(!openForm);
   }
+  function openModal() {
+    setIsOpen(true);
+  }
+ 
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    //subtitle.style.color = '#f00';
+  }
+  // Modal.setAppElement('#yourAppElement')
 
+  function closeModal(){
+    setIsOpen(false);
+  }
   return (
     <div className="App">
     <h1>Projects</h1>
     <Button size='mini' className="grey-btn"onClick={addProject}>Add Project</Button>
     {/* <button onClick={addProject}>Add Project</button> */}
+    <div>
+        <button onClick={openModal}>Open Modal</button>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+ 
+          {/* <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2> */}
+          <button onClick={closeModal}>close</button>
+          <div>
+           Add new Project
+          </div>
+          <AddProject />
+        </Modal>
+      </div>
+
     {openForm?<AddProject />:null}
     
     <div style={{flexDirection:'row',display: 'flex'}}>
