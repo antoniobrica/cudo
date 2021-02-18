2ce813fe8eaab3ff1495339820d18a118ede83d6
dushyant.ohri <dushyant.ohri@softobiz.com>
2021-02-18 17:16:01 +0530

    working on submenu changes

--- apps/ui-design/src/app/app.tsx
+++ apps/ui-design/src/app/app.tsx
@@ -19,7 +19,7 @@ export function App() {
    <Menubar></Menubar> 
    <AccordionExampleMenu></AccordionExampleMenu>
    <Tabsbar></Tabsbar>
-    <ModalExampleModal></ModalExampleModal>  
+    <ModalExampleModal></ModalExampleModal> <br/> <br/><br/>  
     <Cardbar></Cardbar> 
     <Messagebar></Messagebar><br/>
     <Loginbar></Loginbar>
--- libs/shared-components/src/lib/components/menu/sidebar.tsx
+++ libs/shared-components/src/lib/components/menu/sidebar.tsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
  
 import '../../../style/index.scss';
-import {   Menu,  Sidebar, Accordion, Form } from 'semantic-ui-react'
+import {   Icon,  Sidebar, Accordion, Form } from 'semantic-ui-react'
 const ColorForm = (
     <Form>
       <Form.Group grouped> 
@@ -38,72 +38,147 @@ export default class AccordionExampleMenu extends Component {
 
           
         <div className="sidebar-wrapper sidebar-theme">
-            
-        <nav id=" ">
-            <div className="shadow-bottom"></div>
-            <ul className="list-unstyled menu-categories ps ps--active-y" id="accordionExample">
-                <li className="menu">
-                    <a href=" " data-active="true" data-toggle="collapse" aria-expanded="true" className="dropdown-toggle">
-                        <div className="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                            <span>Dashboard</span>
-                        </div>
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
-                        </div>
-                    </a>
-                    <ul className="submenu list-unstyled collapse show" id="dashboard" data-parent="#accordionExample"  >
-                        <li className="active">
-                            <a href="index.html"> Sales </a>
-                        </li>
-                        <li>
-                            <a href="index2.html"> Analytics </a>
-                        </li>
-                    </ul>
-                </li>
+            <Accordion className="ui-accordion">
+        <Accordion.Title
+          active={activeIndex === 0}
+          index={0}
+          onClick={this.handleClick}
+          className="active-title"
+        >
+         
+         Everything <Icon name='angle down' />
+        </Accordion.Title>
+        <Accordion.Content  active={activeIndex === 0}>
+          <ul>
+          <li>Strategic Planning </li>
+          <li>Preliminary Studies</li>
+          <li>Project Planning</li>
+          <li>Tender</li>
+          <li>Realization</li>
+          </ul>
+        </Accordion.Content>
 
-             
-               
-            </ul>
-            
-            
-        </nav>
+        <Accordion.Title
+          active={activeIndex === 1}
+          index={1}
+          onClick={this.handleClick}
+          className="active-title"
+        > 
+          Electrical Work (1/5) <Icon name='angle down' />
+        </Accordion.Title>
+        <Accordion.Content className="active-title" active={activeIndex === 1}>
+        <ul>
+          <li>Strategic Planning </li>
+          <li>Preliminary Studies</li>
+          <li>Project Planning</li>
+          <li>Tender</li>
+          <li>Realization</li>
+          </ul>
+        </Accordion.Content>
+
+        <Accordion.Title
+          active={activeIndex === 2}
+          index={2}
+          onClick={this.handleClick}
+          className="active-title"
+        >
+          
+         HVAC Work <Icon name='angle down' />
+        </Accordion.Title>
+        <Accordion.Content className="active-title" active={activeIndex === 2}>
+        <ul>
+          <li>Strategic Planning </li>
+          <li>Preliminary Studies</li>
+          <li>Project Planning</li>
+          <li>Tender</li>
+          <li>Realization</li>
+          </ul>
+        </Accordion.Content>
+
+        <Accordion.Title
+          active={activeIndex === 3}
+          index={3}
+          onClick={this.handleClick}
+          className="active-title"
+        >
+          
+         Internet wire fitting   <Icon name='angle down' />
+        </Accordion.Title>
+        <Accordion.Content className="active-title" active={activeIndex === 3}>
+        <ul>
+          <li>Strategic Planning </li>
+          <li>Preliminary Studies</li>
+          <li>Project Planning</li>
+          <li>Tender</li>
+          <li>Realization</li>
+          </ul>
+        </Accordion.Content>
+        
+        <Accordion.Title
+          active={activeIndex === 4}
+          index={4}
+          onClick={this.handleClick}
+          className="active-title"
+        >
+           
+         Paint Work  <Icon name='angle down' />
+        </Accordion.Title>
+        <Accordion.Content className="active-title" active={activeIndex === 4}>
+        <ul>
+          <li>Strategic Planning </li>
+          <li>Preliminary Studies</li>
+          <li>Project Planning</li>
+          <li>Tender</li>
+          <li>Realization</li>
+          </ul>
+        </Accordion.Content>
+        
+        <Accordion.Title
+          active={activeIndex === 5}
+          index={5}
+          onClick={this.handleClick}
+          className="active-title"
+        >
+          
+         Paint Work  <Icon name='angle down' />
+        </Accordion.Title>
+        <Accordion.Content className="active-title" active={activeIndex === 5}>
+        <ul>
+          <li>Strategic Planning </li>
+          <li>Preliminary Studies</li>
+          <li>Project Planning</li>
+          <li>Tender</li>
+          <li>Realization</li>
+          </ul>
+        </Accordion.Content>
+        
+        <Accordion.Title
+          active={activeIndex === 6}
+          index={6}
+          onClick={this.handleClick}
+          className="active-title"
+        >
+         
+         Water storage plant    <Icon name='angle down' />
+        </Accordion.Title>
+        <Accordion.Content className="active-title" active={activeIndex === 6}>
+        <ul>
+          <li>Strategic Planning </li>
+          <li>Preliminary Studies</li>
+          <li>Project Planning</li>
+          <li>Tender</li>
+          <li>Realization</li>
+          </ul>
+         
+        </Accordion.Content>
+      </Accordion>
 
     </div>
 
 
 
 
-
-//           <div id="side-menu">
-             
-// <Accordion as={Menu} vertical>
-// <div>
-// <i aria-hidden="true" className="long arrow alternate left"></i>  <span className="preliminary-font">  Burj Khalifa</span>
-//                    <span className="summary float-right"><span className="dot">...</span></span>
-//                   </div>
-//           <Menu.Item>
-//             <Accordion.Title
-//               active={activeIndex === 0}
-//               content='Everything'
-//               index={0}
-//               onClick={this.handleClick}
-//             />
-//             <Accordion.Content active={activeIndex === 0} content={SizeForm} />
-//           </Menu.Item>
-  
-//           <Menu.Item>
-//             <Accordion.Title
-//               active={activeIndex === 1}
-//               content='Colors'
-//               index={1}
-//               onClick={this.handleClick}
-//             />
-//             <Accordion.Content active={activeIndex === 1} content={ColorForm} />
-//           </Menu.Item>
-//         </Accordion>
-
-//           </div>
+ 
         
       )
     }
