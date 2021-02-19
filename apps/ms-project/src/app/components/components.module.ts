70268a58442fdf4b9e1777a240b2b9506fe36466
Ashutosh Mishra <ashutosh.mishra@softobiz.com>
2021-02-19 19:24:52 +0530

    Client Company and Printing Company

--- apps/ms-project/src/app/components/components.module.ts
+++ apps/ms-project/src/app/components/components.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { CompanyModule } from './companies/company.module';
 import { ProjectsModule } from './projects/projects.module';
 import { ReferenceModule } from './reference/reference.module';
 import { WorkTypesModule } from './workTypes/workTypes.module';
@@ -7,6 +8,7 @@ import { WorkTypesModule } from './workTypes/workTypes.module';
     ReferenceModule,
     ProjectsModule,
     WorkTypesModule,
+    CompanyModule,
   ],
   providers: []
 })
