155829bd5c0b0cf1f5ee1d171fc770ea1793a2d6
Vipin Kumar <vipin.kumar@softobiz.com>
2021-03-09 09:40:17 +0530

    updated login flow

--- apps/ms-document/src/app/components/components.module.ts
+++ apps/ms-document/src/app/components/components.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { BlobModule } from './file/blob.module';
+import { BlobModule } from './file/blobStorage/blob.module';
 import { ReferenceModule } from './reference/reference.module';
 
 @Module({
