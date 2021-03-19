4565a9e5607317661f67b6cfcefe1f08cbe1ccdb
dishant.fapot <dishant.fapot@softobiz.com>
2021-03-19 22:33:16 +0530

    Minor Changes

--- apps/ms-document/src/app/components/file/file.module.ts
+++ apps/ms-document/src/app/components/file/file.module.ts
@@ -1,9 +1,9 @@
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { FileEntity } from '../../entities/file.entity';
 import { FileResolver } from './resolver/file.resolver';
 import { FileService } from './service/file.service';
+import { FileEntity } from '../../entities/file.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([FileEntity])],
