08d96e30226b2f35a11fae888a873c383e1713b9
dishant.fapot <dishant.fapot@softobiz.com>
2021-03-22 11:27:20 +0530

    Changes resolved

--- apps/ms-account/src/app/components/files/file-upload.module.ts
+++
@@ -1,9 +0,0 @@
-// import { Module } from '@nestjs/common';
-// import { FileUploadService } from './file-upload.service';
-
-
-// @Module({
-//   controllers: [],
-//   providers: [FileUploadService]
-// })
-// export class FileUploadModule { }
