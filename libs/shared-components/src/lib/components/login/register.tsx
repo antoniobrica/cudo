1d1a5bc02ff92e0425782e6c898e93eb5f3b76c9
dushyant.ohri <dushyant.ohri@softobiz.com>
2021-03-25 15:34:18 +0530

    indented the pages

--- libs/shared-components/src/lib/components/login/register.tsx
+++ libs/shared-components/src/lib/components/login/register.tsx
@@ -1,187 +1,187 @@
 import React from 'react';
- 
+
 import '../../../style/index.scss';
-import {Select, Input, Segment,Form, Grid ,Image,Checkbox,Button,Icon } from 'semantic-ui-react'
+import { Select, Input, Segment, Form, Grid, Image, Checkbox, Button, Icon } from 'semantic-ui-react'
 import logo from 'libs/shared-components/src/slider.png';
 import img from 'libs/shared-components/src/Shape 2.png';
 /* eslint-disable-next-line */
 export interface LoginProps { }
 
 export function RegisterPage(props: LoginProps) {
- 
-    const description = [
-         
-      ] 
-      const countryOptions = [
-        { key: 'af', value: 'af', text: 'Afghanistan' },
-        { key: 'ax', value: 'ax', text: 'Aland Islands' },
-         
-      ]
+
+  const description = [
+
+  ]
+  const countryOptions = [
+    { key: 'af', value: 'af', text: 'Afghanistan' },
+    { key: 'ax', value: 'ax', text: 'Aland Islands' },
+
+  ]
   return (
     <div className="app-content-body ">
-        
-        <div className="main-outer-area">
- 
-            <Grid className="ui-login" columns={1} >
-                    
-            <Grid.Row  >
-      <Grid.Column >
-            
-             <div className="ln-form-outer">
-             
-             <img src={img}   />
-                    <div className="form-header">
-                     
-                    <h2 className="login">Register your account</h2>
-                    
-                    </div>
 
-                    <div className="form-inner">
+      <div className="main-outer-area">
 
-                    <Form>
-<Grid columns={2}>
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-      <label>First Name </label>
-      <Input placeholder='Firstname' size='small' className="full-width" type="text" />
-    </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>Last Name </label>
-      <Input placeholder='Last Name ' size='small' className="full-width" type="text" />
-    </Form.Field>
-  </Grid.Column>
-</Grid.Row>
-</Grid>
-<Grid columns={2}>
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-      <label>Email Address</label>
-      <Input placeholder='Email Address' size='small' className="full-width  " type="text" />
-    </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>Phone Number </label>
-      <Grid columns={2}>
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-    <Select placeholder='Select' className="small" options={countryOptions} /> 
-      </Form.Field>
-      </Grid.Column>
-      <Grid.Column>
-    <Form.Field>
-     <Input placeholder='Phone Number ' size='small' className="full-width" type="text" />
-      </Form.Field>
-      </Grid.Column>
-      </Grid.Row>
-      </Grid>
-    
-    </Form.Field>
-  </Grid.Column>
-</Grid.Row>
-</Grid>
-<Grid columns={2}>
-<Grid.Row>
-    <Grid.Column>
-    <Form.Field>
-      <label> Password</label>
-      <Input placeholder='new password' size='small' className="full-width  " type="password" />
-    </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>Re-Enter Password</label>
-      <Input placeholder='Re-enter password' size='small' className="full-width  " type="password" />
-    </Form.Field>
-  </Grid.Column>
- </Grid.Row>
-</Grid>
-<Grid >
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-      <label>Company Name</label>
-      <Input placeholder='Company Name' size='small' className="full-width  " type="text" />
-    </Form.Field>
-  </Grid.Column>
- </Grid.Row>
-</Grid>
-<Grid columns={2}>
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-      <label>Address Line 1</label>
-      <Input placeholder='Address Line 1 ' size='small' className="full-width" type="text" />
-    </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>Address Line 2</label>
-      <Input placeholder='Address Line 2' size='small' className="full-width" type="text" />
-    </Form.Field>
-  </Grid.Column>
-</Grid.Row>
-</Grid>
+        <Grid className="ui-login" columns={1} >
 
-<Grid columns={4}>
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-      <label>City</label>
-      <Input placeholder='City ' size='small' className="full-width" type="text" />
-    </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>State</label>
-      <Input placeholder='State' size='small' className="full-width" type="text" />
-    </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>Zip</label>
-      <Input placeholder='Zip' size='small' className="full-width" type="text" />
-    </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>Country</label>
-     
-    <Select placeholder='Select' className="small" options={countryOptions} />
-    </Form.Field>
-  </Grid.Column>
-</Grid.Row>
-</Grid>
-<button className="ui large button grey-btn btn-large">Register <i aria-hidden="true" className="arrow right icon"></i>   </button>
+          <Grid.Row  >
+            <Grid.Column >
+
+              <div className="ln-form-outer">
+
+                <img src={img} />
+                <div className="form-header">
+
+                  <h2 className="login">Register your account</h2>
+
+                </div>
+
+                <div className="form-inner">
+
+                  <Form>
+                    <Grid columns={2}>
+                      <Grid.Row>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>First Name </label>
+                            <Input placeholder='Firstname' size='small' className="full-width" type="text" />
+                          </Form.Field>
+                        </Grid.Column>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>Last Name </label>
+                            <Input placeholder='Last Name ' size='small' className="full-width" type="text" />
+                          </Form.Field>
+                        </Grid.Column>
+                      </Grid.Row>
+                    </Grid>
+                    <Grid columns={2}>
+                      <Grid.Row>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>Email Address</label>
+                            <Input placeholder='Email Address' size='small' className="full-width  " type="text" />
+                          </Form.Field>
+                        </Grid.Column>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>Phone Number </label>
+                            <Grid columns={2}>
+                              <Grid.Row>
+                                <Grid.Column>
+                                  <Form.Field>
+                                    <Select placeholder='Select' className="small" options={countryOptions} />
+                                  </Form.Field>
+                                </Grid.Column>
+                                <Grid.Column>
+                                  <Form.Field>
+                                    <Input placeholder='Phone Number ' size='small' className="full-width" type="text" />
+                                  </Form.Field>
+                                </Grid.Column>
+                              </Grid.Row>
+                            </Grid>
+
+                          </Form.Field>
+                        </Grid.Column>
+                      </Grid.Row>
+                    </Grid>
+                    <Grid columns={2}>
+                      <Grid.Row>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label> Password</label>
+                            <Input placeholder='new password' size='small' className="full-width  " type="password" />
+                          </Form.Field>
+                        </Grid.Column>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>Re-Enter Password</label>
+                            <Input placeholder='Re-enter password' size='small' className="full-width  " type="password" />
+                          </Form.Field>
+                        </Grid.Column>
+                      </Grid.Row>
+                    </Grid>
+                    <Grid >
+                      <Grid.Row>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>Company Name</label>
+                            <Input placeholder='Company Name' size='small' className="full-width  " type="text" />
+                          </Form.Field>
+                        </Grid.Column>
+                      </Grid.Row>
+                    </Grid>
+                    <Grid columns={2}>
+                      <Grid.Row>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>Address Line 1</label>
+                            <Input placeholder='Address Line 1 ' size='small' className="full-width" type="text" />
+                          </Form.Field>
+                        </Grid.Column>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>Address Line 2</label>
+                            <Input placeholder='Address Line 2' size='small' className="full-width" type="text" />
+                          </Form.Field>
+                        </Grid.Column>
+                      </Grid.Row>
+                    </Grid>
+
+                    <Grid columns={4}>
+                      <Grid.Row>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>City</label>
+                            <Input placeholder='City ' size='small' className="full-width" type="text" />
+                          </Form.Field>
+                        </Grid.Column>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>State</label>
+                            <Input placeholder='State' size='small' className="full-width" type="text" />
+                          </Form.Field>
+                        </Grid.Column>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>Zip</label>
+                            <Input placeholder='Zip' size='small' className="full-width" type="text" />
+                          </Form.Field>
+                        </Grid.Column>
+                        <Grid.Column>
+                          <Form.Field>
+                            <label>Country</label>
+
+                            <Select placeholder='Select' className="small" options={countryOptions} />
+                          </Form.Field>
+                        </Grid.Column>
+                      </Grid.Row>
+                    </Grid>
+                    <button className="ui large button grey-btn btn-large">Register <i aria-hidden="true" className="arrow right icon"></i>   </button>
 
 
-    </Form>
-                 </div>  
+                  </Form>
+                </div>
 
               </div>
-        
+
             </Grid.Column>
 
-             
-           
-            </Grid.Row>
-             
-            </Grid>
- 
-          
-        </div>
 
-        
+
+          </Grid.Row>
+
+        </Grid>
+
+
+      </div>
+
+
 
     </div>
-        
-    
- 
+
+
+
   );
 }
 
--- libs/shared-components/src/lib/components/userprofile/userprofileview.tsx
+++ libs/shared-components/src/lib/components/userprofile/userprofileview.tsx
@@ -1,185 +1,184 @@
 import { radios } from '@storybook/addon-knobs';
 import React from 'react';
-import { Button, Select,   Modal,  Tab,Table,  Input, Form, Grid, Image, Segment, TextArea} from 'semantic-ui-react';
+import { Button, Select, Modal, Tab, Table, Input, Form, Grid, Image, Segment, TextArea } from 'semantic-ui-react';
 // import SampleModal from './sample-modal';
- 
-import img from 'libs/shared-components/src/user_profile.png'; 
+
+import img from 'libs/shared-components/src/user_profile.png';
 
 
 function UserProfileView() {
-    const countryOptions = [
-        { key: 'af', value: 'af', text: 'Afghanistan' },
-        { key: 'ax', value: 'ax', text: 'Aland Islands' },
-         
-      ]
+  const countryOptions = [
+    { key: 'af', value: 'af', text: 'Afghanistan' },
+    { key: 'ax', value: 'ax', text: 'Aland Islands' },
 
- 
+  ]
+
+
   const [open, setOpen] = React.useState(false)
-   
+
   return (
-    <div className="app-content-body-dash navbar-collapse box-shadow bg-white-only" style={{background:'#e6e6e6'}}>
-      <div style={{background:'#fff', padding:'10px'}}> 
-          <span className="preliminary-font">User Profile</span>
-      
-         </div>
-         <Grid columns={4}>
-    <Grid.Column>
-     
-  
-  <div className="card card-custom gutter-b">
- 
- <div className="card-body">
- <img src={img}   /><br/>
- <label className="text-center">Change Picture</label>
- <Input  type="file" className="file-upload-input"   placeholder='Firstname' />
-  
- </div>
-  
-    
- 
-</div>
+    <div className="app-content-body-dash navbar-collapse box-shadow bg-white-only" style={{ background: '#e6e6e6' }}>
+      <div style={{ background: '#fff', padding: '10px' }}>
+        <span className="preliminary-font">User Profile</span>
 
-    </Grid.Column>
-    <Grid.Column>
-
-    <div className="card card-custom gutter-b widthcard" >
-  
- <div className="card-body">
-<Form>
-<Grid columns={2}>
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-      <label>First Name </label>
-      <label>John  </label> 
-    </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>Last Name </label>
-      <label>  Smith  </label> 
-    </Form.Field>
-  </Grid.Column>
-</Grid.Row>
-</Grid>
-<Grid columns={2}>
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-      <label>Email Address</label>
-       <label>johnsmith@gmail.com </label> 
-    </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>Phone Number </label>
-      <label>+1 987 325 9875 </label> 
-       </Form.Field>
-  </Grid.Column>
-</Grid.Row>
-</Grid>
-<Grid columns={2}>
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-      <label>Old Passord</label>
-      <label>1111d</label>
-    </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>New Passord</label>
-     <label>1111d</label>
-    </Form.Field>
-  </Grid.Column>
-   
- </Grid.Row>
-</Grid>
-<Grid >
- <Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-      <label>Company Name</label>
-      <label>Kiran Construction Co</label>
-     </Form.Field>
-  </Grid.Column>
- </Grid.Row>
-</Grid>
-<Grid columns={2}>
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-      <label>Address Line 1</label>
-     
-      <label> 532 68th Street</label>
-    </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>Address Line 2</label>
-       <label> </label>
-    </Form.Field>
-  </Grid.Column>
-</Grid.Row>
-</Grid>
-
-<Grid columns={4}>
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-      <label>City</label>
-      <label>New York</label>
-     </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>State</label>
-      <label>NY</label>
-      </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>Zip</label>
-      <label>10605-78 156 </label>
-      </Form.Field>
-  </Grid.Column>
-  <Grid.Column>
-    <Form.Field>
-      <label>Country</label>
-      <label>USA</label>
-     </Form.Field>
-  </Grid.Column>
-</Grid.Row>
-</Grid>
-<Grid columns={1}>
-<Grid.Row>
-  <Grid.Column>
-    <Form.Field>
-    <Button
-          content="Edit Profile" 
-          
-          size='mini' className="disabled"
-        />
-         
-    </Form.Field>
-  </Grid.Column>
- 
-</Grid.Row>
-</Grid>
+      </div>
+      <Grid columns={4}>
+        <Grid.Column>
 
 
-    </Form>
- </div>
-</div>
-   
-    </Grid.Column>
-  </Grid>
-   
-  </div>
+          <div className="card card-custom gutter-b">
 
-  
+            <div className="card-body">
+              <img src={img} /><br />
+              <label className="text-center">Change Picture</label>
+              <Input type="file" className="file-upload-input" placeholder='Firstname' />
+
+            </div>
+
+
+
+          </div>
+
+        </Grid.Column>
+        <Grid.Column>
+
+          <div className="card card-custom gutter-b widthcard" >
+
+            <div className="card-body">
+              <Form>
+                <Grid columns={2}>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>First Name </label>
+                        <label>John  </label>
+                      </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>Last Name </label>
+                        <label>  Smith  </label>
+                      </Form.Field>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+                <Grid columns={2}>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>Email Address</label>
+                        <label>johnsmith@gmail.com </label>
+                      </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>Phone Number </label>
+                        <label>+1 987 325 9875 </label>
+                      </Form.Field>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+                <Grid columns={2}>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>Old Passord</label>
+                        <label>1111d</label>
+                      </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>New Passord</label>
+                        <label>1111d</label>
+                      </Form.Field>
+                    </Grid.Column>
+
+                  </Grid.Row>
+                </Grid>
+                <Grid >
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>Company Name</label>
+                        <label>Kiran Construction Co</label>
+                      </Form.Field>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+                <Grid columns={2}>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>Address Line 1</label>
+
+                        <label> 532 68th Street</label>
+                      </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>Address Line 2</label>
+                        <label> </label>
+                      </Form.Field>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+
+                <Grid columns={4}>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>City</label>
+                        <label>New York</label>
+                      </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>State</label>
+                        <label>NY</label>
+                      </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>Zip</label>
+                        <label>10605-78 156 </label>
+                      </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Form.Field>
+                        <label>Country</label>
+                        <label>USA</label>
+                      </Form.Field>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+                <Grid columns={1}>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Form.Field>
+                        <Button
+                          content="Edit Profile"
+
+                          size='mini' className="disabled"
+                        />
+
+                      </Form.Field>
+                    </Grid.Column>
+
+                  </Grid.Row>
+                </Grid>
+
+
+              </Form>
+            </div>
+          </div>
+
+        </Grid.Column>
+      </Grid>
+
+    </div>
+
+
   )
 }
 
 export default UserProfileView
- 
