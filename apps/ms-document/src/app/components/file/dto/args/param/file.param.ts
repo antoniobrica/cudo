ac58e3e0f66f4dc882fcff95a0414204e4433b58
Ashutosh Mishra <ashutosh.mishra@softobiz.com>
2021-04-12 13:28:36 +0530

    File Version is added

--- apps/ms-document/src/app/components/file/dto/args/param/file.param.ts
+++ apps/ms-document/src/app/components/file/dto/args/param/file.param.ts
@@ -2,13 +2,16 @@ import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class FileParams {
-  @Field({ description: `User ID` })
+  @Field({ description: `File URL` })
   fileURL?: string;
 
-  @Field({ description: `User Name` })
+  @Field({ description: `File title` })
   fileTitle?: string;
 
-  @Field({ description: `User Name` })
+  @Field({ description: `File Type` })
   fileType?: string;
+
+  @Field({ description: `File Version` })
+  fileVersion?: string;
 
 }
