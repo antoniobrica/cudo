8730ae775f449b6ea4ba5a5a11bed049e10c2bf8
Ashutosh Mishra <ashutosh.mishra@softobiz.com>
2021-06-11 13:57:06 +0530

    All URL moved in one file MS_SERVICE_URL

--- apps/mf-container-app/src/environments/environment.ts
+++ apps/mf-container-app/src/environments/environment.ts
@@ -1,8 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // When building for production, this file is replaced with `environment.prod.ts`.
 
+import { MS_SERVICE_URL } from "@cudo/mf-core";
+
 export const environment = {
   production: false,
-  REACT_APP_PROJECT_HOST: "http://192.168.0.31:6006",
-  MS_ACCOUNT_URL: "http://cudo-ms-account.softobiz.net/graphql"
+  REACT_APP_PROJECT_HOST: MS_SERVICE_URL['mf_project_app'].url,
+  MS_ACCOUNT_URL: MS_SERVICE_URL['ms_account'].url
 };
--- libs/mf-core/src/lib/constants/ms-urls.tsx
+++ libs/mf-core/src/lib/constants/ms-urls.tsx
@@ -14,5 +14,36 @@ export const MS_SERVICE_URL: { [serviceName: string]: IURL } = {
   },
   "ms_task": {
     url: "http://cudo-ms-task.softobiz.net/graphql"
+  },
+  "ms_cost": {
+    url: 'http://192.168.29.131:5002/graphql'
+  },
+  "mf_cost_app": {
+    url: "http://192.168.29.131:6004"
+  },
+  "mf_meeting_app": {
+    url: "http://192.168.29.131:6005"
+  },
+  "mf_project_app": {
+    url: "http://192.168.29.131:6006"
+  },
+  "mf_task_app": {
+    url: "http://192.168.29.131:6009"
+  },
+  "mf_document_app": {
+    url: "http://192.168.29.131:6010"
+  },
+  "kratos_BASE_URL": {
+    url: "http://192.168.29.131:4455"
+  },
+  "kratos_KRATOS_BROWSER_URL": {
+    url: "http://192.168.29.131:4455/.ory/kratos/public"
+  },
+  "kratos_KRATOS_PUBLIC_URL": {
+    url: "http://192.168.29.131:4455/.ory/kratos/public"
+  },
+  "kratos_KRATOS_ADMIN_URL": {
+    url: "http://192.168.29.131:4434"
   }
+
 }
