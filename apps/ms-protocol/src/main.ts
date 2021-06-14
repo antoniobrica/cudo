cc12532882fbb48bd1f455aeddb29a0ab24e6497
Vipin Kumar <vipin.kumar@softobiz.com>
2021-06-14 20:53:45 +0530

    updated ip address

--- apps/ms-protocol/src/main.ts
+++ apps/ms-protocol/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
-    Logger.log('Listening at http://192.168.1.5:' + port + '/' + globalPrefix);
+    Logger.log('Listening at http://192.168.0.31:' + port + '/' + globalPrefix);
   });
 }
 
