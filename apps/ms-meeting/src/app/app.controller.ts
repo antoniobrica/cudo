014694c1005737943a76ac0277632b01ca5fa4c9
Ashutosh Mishra <ashutosh.mishra@softobiz.com>
2021-07-08 10:43:55 +0530

    URL Corrected

--- apps/ms-meeting/src/app/app.controller.ts
+++ apps/ms-meeting/src/app/app.controller.ts
@@ -6,8 +6,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getData("health-check") {
+  @Get("health-check")
+  getData() {
     return this.appService.getData();
   }
 }
