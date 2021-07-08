5cf6e989370bd926d1972ac9999ee2b9cf53f59b
Ashutosh Mishra <ashutosh.mishra@softobiz.com>
2021-07-08 09:22:08 +0530

    Health Check API added in ms-meeting

--- apps/ms-meeting/src/app/app.controller.ts
+++ apps/ms-meeting/src/app/app.controller.ts
@@ -4,10 +4,10 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getData() {
+  getData("health-check") {
     return this.appService.getData();
   }
 }
