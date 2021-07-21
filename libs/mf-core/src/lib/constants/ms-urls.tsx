a4168374d5b323a9675f774e9a76515d7431d8c2
mukutSoftobiz <Mukut.Kandar@softobiz.com>
2021-07-21 17:19:55 +0530

    Modified configuration files and delete package lock file for generate pull request

--- libs/mf-core/src/lib/constants/ms-urls.tsx
+++ libs/mf-core/src/lib/constants/ms-urls.tsx
@@ -4,7 +4,6 @@ interface IURL {
 export const MS_SERVICE_URL: { [serviceName: string]: IURL } = {
   "ms_account": {
     url: "http://dev.cudo.ch/api/ms-account/graphql"
-    // url: "http://192.168.3.198:5001/graphql"
   },
   "ms_document": {
     url: "http://dev.cudo.ch/api/ms-document/graphql"
@@ -19,38 +18,34 @@ export const MS_SERVICE_URL: { [serviceName: string]: IURL } = {
     url: "http://dev.cudo.ch/api/ms-cost/graphql"
   },
   "ms_meeting": {
-    // url: "http://dev.cudo.ch/api/ms-meeting/graphql"
-    url: "http://192.168.3.198:5004/graphql"
+    url: "http://dev.cudo.ch/api/ms-meeting/graphql"
   },
   "mf_cost_app": {
-    url: "http://192.168.3.198:6004"
+    url: "http://cudo-mf_cost.softobiz.net"
   },
   "mf_meeting_app": {
-    url: "http://192.168.3.198:6005"
+    url: "http://cudo-mf_meeting.softobiz.net"
   },
   "mf_project_app": {
-    url: "http://192.168.3.198:6006"
+    url: "http://cudo-mf-project.softobiz.net"
   },
   "mf_task_app": {
-    url: "http://192.168.3.198:6009"
+    url: "http://cudo-mf-task.softobiz.net"
   },
   "mf_document_app": {
-    url: "http://192.168.3.198:6010"
+    url: "http://cudo-mf-document.softobiz.net"
   },
   "kratos_BASE_URL": {
-    url: "http://192.168.3.198:4455"
-    // url: "http://cudo-ory.symple.co.in"
+    url: "http://cudoapp.softobiz.net"
   },
   "kratos_KRATOS_BROWSER_URL": {
-    url: "http://192.168.3.198:4455/.ory/kratos/public"
-    // url: "http://cudo-ory.symple.co.in/.ory/kratos/public"
+    url: "http://cudoapp.softobiz.net/.ory/kratos/public"
   },
   "kratos_KRATOS_PUBLIC_URL": {
-    url: "http://192.168.3.198:4455/.ory/kratos/public"
-    // url: "http://cudo-ory.symple.co.in/.ory/kratos/public"
+    url: "http://cudoapp.softobiz.net/.ory/kratos/public"
   },
   "kratos_KRATOS_ADMIN_URL": {
-    url: "http://192.168.3.198:4434"
+    url: "http://192.168.0.31:4434"
   },
   "ASSETS_CDN_URL": {
     url: "https://cudodevcdn.azureedge.net"
