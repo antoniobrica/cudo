2ec769acd25c593e014ca8d2a400f162f38ff3f1
mukutSoftobiz <Mukut.Kandar@softobiz.com>
2021-07-23 20:46:59 +0530

    Modified ms-url for ip change as 198.168.1.5 with cost changes after git pull from development

--- libs/mf-core/src/lib/constants/ms-urls.tsx
+++ libs/mf-core/src/lib/constants/ms-urls.tsx
@@ -21,31 +21,31 @@ export const MS_SERVICE_URL: { [serviceName: string]: IURL } = {
     url: "http://dev.cudo.ch/api/ms-meeting/graphql"
   },
   "mf_cost_app": {
-    url: "http://192.168.1.5:6004"
+    url: "http://cudo-mf_cost.softobiz.net"
   },
   "mf_meeting_app": {
-    url: "http://192.168.1.5:6005"
+    url: "http://cudo-mf_meeting.softobiz.net"
   },
   "mf_project_app": {
-    url: "http://192.168.1.5:6006"
+    url: "http://cudo-mf-project.softobiz.net"
   },
   "mf_task_app": {
-    url: "http://192.168.1.5:6009"
+    url: "http://cudo-mf-task.softobiz.net"
   },
   "mf_document_app": {
-    url: "http://192.168.1.5:6010"
+    url: "http://cudo-mf-document.softobiz.net"
   },
   "kratos_BASE_URL": {
-    url: "http://192.168.1.5:4455"
+    url: "http://cudoapp.softobiz.net"
   },
   "kratos_KRATOS_BROWSER_URL": {
-    url: "http://192.168.1.5:4455/.ory/kratos/public"
+    url: "http://cudoapp.softobiz.net/.ory/kratos/public"
   },
   "kratos_KRATOS_PUBLIC_URL": {
-    url: "http://192.168.1.5:4455/.ory/kratos/public"
+    url: "http://cudoapp.softobiz.net/.ory/kratos/public"
   },
   "kratos_KRATOS_ADMIN_URL": {
-    url: "http://192.168.1.5:4434"
+    url: "http://192.168.0.31:4434"
   },
   "ASSETS_CDN_URL": {
     url: "https://cudodevcdn.azureedge.net"
