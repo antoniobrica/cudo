f134902092cf2e7b17124684f949cf6494163e52
amit <amit.kumar@softobiz.com>
2021-08-09 13:04:23 +0530

    update ms-urls

--- libs/mf-core/src/lib/constants/ms-urls.tsx
+++ libs/mf-core/src/lib/constants/ms-urls.tsx
@@ -5,52 +5,47 @@ export const MS_SERVICE_URL: { [serviceName: string]: IURL } = {
   "ms_account": {
     url: "http://dev.cudo.ch/api/ms-account/graphql"
   },
- 
   "ms_document": {
     url: "http://dev.cudo.ch/api/ms-document/graphql"
   },
- 
   "ms_project": {
     url: "http://dev.cudo.ch/api/ms-project/graphql"
   },
- 
   "ms_task": {
     url: "http://dev.cudo.ch/api/ms-task/graphql"
   },
- 
   "ms_cost": {
     url: "http://dev.cudo.ch/api/ms-cost/graphql"
   },
- 
   "ms_meeting": {
     url: "http://dev.cudo.ch/api/ms-meeting/graphql"
   },
   "mf_cost_app": {
-    url: "http://192.168.1.5:6004"
+    url: "http://cudo-mf_cost.softobiz.net"
   },
   "mf_meeting_app": {
-    url: "http://192.168.1.5:6005"
+    url: "http://cudo-mf_meeting.softobiz.net"
   },
   "mf_project_app": {
-    url: "http://192.168.1.5:6006"
+    url: "http://cudo-mf-project.softobiz.net"
   },
   "mf_task_app": {
-    url: "http://192.168.1.5:6009"
+    url: "http://cudo-mf-task.softobiz.net"
   },
   "mf_document_app": {
-    url: "http://192.168.1.5:6010"
+    url: "http://cudo-mf-document.softobiz.net"
   },
   "kratos_BASE_URL": {
-    url: "http://192.168.1.5:4455"
+    url: "http://cudoapp.softobiz.net"
   },
   "kratos_KRATOS_BROWSER_URL": {
-    url: "http://192.168.1.5:4455/.ory/kratos/public"
+    url: "http://cudoapp.softobiz.net/.ory/kratos/public"
   },
   "kratos_KRATOS_PUBLIC_URL": {
-    url: "http://192.168.1.5:4455/.ory/kratos/public"
+    url: "http://cudoapp.softobiz.net/.ory/kratos/public"
   },
   "kratos_KRATOS_ADMIN_URL": {
-    url: "http://192.168.1.5:4434"
+    url: "http://192.168.0.31:4434"
   },
   "ASSETS_CDN_URL": {
     url: "https://cudodevcdn.azureedge.net"
