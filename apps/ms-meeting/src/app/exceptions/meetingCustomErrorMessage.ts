4a25127ca6869a9f12275021423e8f837c7193ac
ajay.kumar <ajay.kumar@softobiz.com>
2021-08-12 12:00:08 +0530

    status code changed

--- apps/ms-meeting/src/app/exceptions/meetingCustomErrorMessage.ts
+++ apps/ms-meeting/src/app/exceptions/meetingCustomErrorMessage.ts
@@ -5,42 +5,44 @@ interface IMeetingErrorCode {
     code: number
 }
 
+const port = process.env.PORT || 3333
+
 export const MeetingErrorData = (errorCode: number): IMeetingErrorCode => {
     switch (errorCode) {
         case MeetingErrorTypeEnum.RECORD_ALREADY_EXIST:
-            return { message: "Record already exists", code: 409 }
+            return { message: "Record already exists", code: +(port + "1") }
         case MeetingErrorTypeEnum.RECORD_NOT_EXIST:
-            return { message: "Record not exist", code: 404 }
+            return { message: "Record not exist", code: +(port + "2") }
         case MeetingErrorTypeEnum.RECORD_NOT_ADDED:
-            return { message: "Record not_added", code: 400 }
+            return { message: "Record not_added", code: +(port + "3") }
         case MeetingErrorTypeEnum.NO_TITLE:
-            return { message: "Can't pass empty Title", code: 400 }
+            return { message: "Can't pass empty Title", code: +(port + "4") }
         case MeetingErrorTypeEnum.NO_WORKTYPE:
-            return { message: "Can't pass empty Worktype", code: 400 }
+            return { message: "Can't pass empty Worktype", code: +(port + "5") }
         case MeetingErrorTypeEnum.NO_PHASE:
-            return { message: "Can't pass empty Phase", code: 400 }
+            return { message: "Can't pass empty Phase", code: +(port + "6") }
         case MeetingErrorTypeEnum.NO_ASSIGNEE:
-            return { message: "Can't pass empty Assignee", code: 400 }
+            return { message: "Can't pass empty Assignee", code: +(port + "7") }
         case MeetingErrorTypeEnum.NO_CATEGORY:
-            return {message: "Can't pass empty Category", code: 400}
+            return {message: "Can't pass empty Category", code: +(port + "8")}
         case MeetingErrorTypeEnum.NO_MEMBERS:
-            return { message: "Can't pass empty Members", code: 400 }
+            return { message: "Can't pass empty Members", code: +(port + "9") }
         case MeetingErrorTypeEnum.WRONG_TIME:
-            return { message: "Can't pass smaller End time", code: 400 }
+            return { message: "Can't pass smaller End time", code: +(port + "10") }
         case MeetingErrorTypeEnum.NO_DATE:
-            return { message: "Can't pass empty Date", code: 400 }
+            return { message: "Can't pass empty Date", code: +(port + "11") }
         case MeetingErrorTypeEnum.NO_INVITATION:
-            return { message: "Can't pass empty Invitation", code: 400 }
+            return { message: "Can't pass empty Invitation", code: +(port + "12") }
         case MeetingErrorTypeEnum.NO_PROTOCOL:
-            return { message: "Can't pass empty Protocol", code: 400 }
+            return { message: "Can't pass empty Protocol", code: +(port + "31") }
         case MeetingErrorTypeEnum.NO_ADMIN:
-            return { message: "Can't pass empty Admin", code: 400 }
+            return { message: "Can't pass empty Admin", code: +(port + "14") }
         case MeetingErrorTypeEnum.NO_TIME:
-            return { message: "Can't pass empty time", code: 400 }
+            return { message: "Can't pass empty time", code: +(port + "15") }
         case MeetingErrorTypeEnum.PROJECT_NOT_FOUND:
-            return { message: "Project not found", code: 404 }
+            return { message: "Project not found", code: +(port + "16") }
         case MeetingErrorTypeEnum.NO_REFERANCE_TYPE:
-            return { message: "Reference type does not exists", code: 404 }
+            return { message: "Reference type does not exists", code: +(port + "17") }
         default:
             return { message: "An internal server error occurred", code: 500 }
     }
