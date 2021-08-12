abcb8e20938fc8fef4727ca8cf38b30eaf3168c8
ajay.kumar <ajay.kumar@softobiz.com>
2021-08-12 12:07:28 +0530

    status code change

--- apps/ms-task/src/app/exceptions/taskCustomeErrorMessage.ts
+++ apps/ms-task/src/app/exceptions/taskCustomeErrorMessage.ts
@@ -5,37 +5,39 @@ interface ITaskErrorCode {
     code: number
 }
 
+const port = process.env.PORT || 3333;
+
 export const TaskErrorData = (errorCode: number): ITaskErrorCode => {
     switch (errorCode) {
         case TaskErrorTypeEnum.RECORD_ALREADY_EXIST:
-            return { message: "Record already exists", code: 409 }
+            return { message: "Record already exists", code: +(port + "1") }
 
         case TaskErrorTypeEnum.RECORD_NOT_EXIST:
-            return { message: "Record not exist", code: 404 }
+            return { message: "Record not exist", code: +(port + "2") }
 
         case TaskErrorTypeEnum.RECORD_NOT_ADDED:
-            return { message: "Record not_added", code: 400 }
+            return { message: "Record not_added", code: +(port + "3") }
 
         case TaskErrorTypeEnum.NO_TITLE:
-            return { message: "Can't pass empty Title", code: 400 }
+            return { message: "Can't pass empty Title", code: +(port + "4") }
 
         case TaskErrorTypeEnum.NO_WORKTYPE:
-            return { message: "Can't pass empty Worktype", code: 400 }
+            return { message: "Can't pass empty Worktype", code: +(port + "5") }
 
         case TaskErrorTypeEnum.NO_PHASE:
-            return { message: "Can't pass empty Phase", code: 400 }
+            return { message: "Can't pass empty Phase", code: +(port + "6") }
 
         case TaskErrorTypeEnum.NO_ASSIGNEE:
-            return { message: "Can't pass empty Assignee", code: 400 }
+            return { message: "Can't pass empty Assignee", code: +(port + "7") }
 
         case TaskErrorTypeEnum.WRONG_DATE:
-            return { message: "Can't pass smaller End Date", code: 400 }
+            return { message: "Can't pass smaller End Date", code: +(port + "8") }
         case TaskErrorTypeEnum.NO_DATE:
-            return { message: "Can't pass empty Date", code: 400 }
+            return { message: "Can't pass empty Date", code: +(port + "9") }
         case TaskErrorTypeEnum.PROJECT_NOT_FOUND:
-            return { message: "Project not found", code: 404 }
+            return { message: "Project not found", code: +(port + "10") }
         case TaskErrorTypeEnum.NO_REFERANCE_TYPE:
-            return { message: "Reference type does not exists", code: 404 }
+            return { message: "Reference type does not exists", code: +(port + "11") }
         default:
             return { message: "An internal server error occurred", code: 500 }
     }
