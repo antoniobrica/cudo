0601fb1629f4fdbff3b49eb1d09f5f25cda03aab
amit <amit.kumar@softobiz.com>
2021-09-17 16:17:04 +0530

    update configuration files

--- libs/mf-core/src/lib/constants/ms-urls.tsx
+++ libs/mf-core/src/lib/constants/ms-urls.tsx
@@ -3,67 +3,51 @@ interface IURL {
 }
 export const MS_SERVICE_URL: { [serviceName: string]: IURL } = {
   "ms_account": {
-    // url: 'http://192.168.1.5:5001/graphql'
-    url: 'https://dev.cudo.ch/api/ms-account/graphql'
+    url: "https://dev.cudo.ch/api/ms-account/graphql"
   },
   "ms_document": {
-    // url: 'http://192.168.1.5:5003/api/ms-document/graphql'
     url: "https://dev.cudo.ch/api/ms-document/graphql"
   },
   "ms_project": {
-    // url: 'http://192.168.1.5:5005/graphql'
     url: "https://dev.cudo.ch/api/ms-project/graphql"
   },
   "ms_task": {
-    // url: 'http://192.168.1.5:5007/graphql'
     url: "https://dev.cudo.ch/api/ms-task/graphql"
   },
   "ms_cost": {
-    // url: 'http://192.168.1.5:5002/graphql'
     url: "https://dev.cudo.ch/api/ms-cost/graphql"
   },
   "ms_meeting": {
-    // url: 'http://192.168.1.5:5004/graphql'
     url: "https://dev.cudo.ch/api/ms-meeting/graphql"
   },
   "mf_cost_app": {
-    url: "http://192.168.1.5:6004"
-    // url: "http://dev.cudo.ch/api/ms-cost/graphql"
+    url: "https://costappdev.cudo.ch"
   },
   "mf_meeting_app": {
-    url: "http://192.168.1.5:6005"
-    // url: "http://cudo-mf_meeting.softobiz.net"
+    url: "https://meetingappdev.cudo.ch"
   },
   "mf_project_app": {
-    url: "http://192.168.1.5:6006"
-    // url: "http://cudo-mf-project.softobiz.net"
+    url: "https://projectappdev.cudo.ch"
   },
   "mf_task_app": {
-    url: "http://192.168.1.5:6009"
-    // url: "http://cudo-mf-task.softobiz.net"
+    url: "https://taskappdev.cudo.ch"
   },
   "mf_document_app": {
-    url: "http://192.168.1.5:6010"
-    // url: "http://cudo-mf-document.softobiz.net"
+    url: "https://documentappdev.cudo.ch"
   },
   "kratos_BASE_URL": {
-    url: "http://192.168.1.5:4455"
-    // url: "http://cudo-ory.symple.co.in"
+    url: "https://development.cudo.ch"
   },
   "kratos_KRATOS_BROWSER_URL": {
-    url: "http://192.168.1.5:4455/.ory/kratos/public"
-    // url: "http://cudo-ory.symple.co.in/.ory/kratos/public"
+    url: "https://development.cudo.ch/.ory/kratos/public"
   },
   "kratos_KRATOS_PUBLIC_URL": {
-    url: "http://192.168.1.5:4455/.ory/kratos/public"
-    // url: "http://cudo-ory.symple.co.in/.ory/kratos/public"
+    url: "https://development.cudo.ch/.ory/kratos/public"
   },
   "kratos_KRATOS_ADMIN_URL": {
-    url: "http://192.168.1.5:4434"
-    // url: "http://192.168.1.5:4434"
+    url: "http://192.168.0.31:4434"
   },
   "ASSETS_CDN_URL": {
     url: "https://cudodevcdn.azureedge.net"
   }
-
 }
